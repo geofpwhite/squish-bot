62baef9c46133ea378e2593aa654b0dbe525f7ca
Geoffrey White <geoffpiercewhite@gmail.com>
2025-11-16 20:22:34 -0500

    name calling bot

--- squish_bot/lib/squish_bot/consumer.ex
+++ squish_bot/lib/squish_bot/consumer.ex
@@ -3,14 +3,33 @@ defmodule SquishBot.Consumer do
 
   alias Nostrum.Api.Message
 
+
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     case msg.content do
-      "!hello" ->
-        {:ok, _message} = Message.create(msg.channel_id, "Hello, world!")
+      "!call " <> rest ->
+        ary = String.split(rest, " ")
 
-      _ ->
-        :ignore
+        {:ok, _message} =
+          Message.create(msg.channel_id, craft_name_calling_message(ary))
+
+      other ->
+        if String.at(other, String.length(other) - 1) == "?" do
+          Message.create(
+            msg.channel_id,
+            "Hey! <@" <>
+              Integer.to_string(msg.author.id) <>
+              ">! Nobody cares about your baby brain smooth brain questions. Bruh."
+          )
+        end
     end
+  end
+
+  def craft_name_calling_message(str_ary) do
+    craft_name_calling_message("Hey! ", str_ary)
+  end
+
+  def craft_name_calling_message(insult, [name | tail]) do
+    insult <> name <> "! You're " <> Enum.join(tail, " ") <> ", bruh."
   end
 
   # Ignore any other events
