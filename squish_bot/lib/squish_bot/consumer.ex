4c573f2c2d7ebedd5181bb0d1f58ec69db0128c4
Geoffrey White <geoffpiercewhite@gmail.com>
2026-01-06 20:57:35 -0500

    adding stuff

--- squish_bot/lib/squish_bot/consumer.ex
+++ squish_bot/lib/squish_bot/consumer.ex
@@ -3,9 +3,44 @@ defmodule SquishBot.Consumer do
 
   alias Nostrum.Api.Message
 
+  def handle_event({:MESSAGE_CREATE, msg, _ws_state})
+      when msg.author.id == 522_631_384_989_433_857 do
+    case msg.content do
+      "!call " <> _ ->
+        Message.create(
+          msg.channel_id,
+          "parker is NOT allowed to use this"
+        )
+    end
+  end
 
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     case msg.content do
+      "!eval " <> rest ->
+        IO.inspect(rest)
+        rest = String.trim(rest,"\n")
+
+        {:ok, pid} = StringIO.open("hold")
+        tok = Stench.Lexer.tokenize(rest)
+        tree = Stench.Parser.parse(tok)
+        Application.put_env(:stench, :buffer, pid)
+        IO.puts(inspect(tok))
+        state = Stench.CLI.eval(rest,true)
+        # state = Stench.Eval.eval(tree)
+        s = Tuple.to_list(StringIO.contents(pid))
+        s = Enum.take(s, 1 - Enum.count(s))
+        s = Enum.join(s, "\n")
+        IO.puts("s: " <> s)
+
+        {:ok, _message} =
+          case state.cur_return.type do
+          :bucket->
+            Message.create(msg.channel_id, s <> Enum.join(Enum.map(state.cur_return.value, fn var -> var.value end),","))
+          _->
+            Message.create(msg.channel_id, s <> to_string(state.cur_return.value))
+
+          end
+
       "!call " <> rest ->
         ary = String.split(rest, " ")
 
@@ -24,12 +59,18 @@ defmodule SquishBot.Consumer do
     end
   end
 
-  def craft_name_calling_message(str_ary) do
+  def craft_name_calling_message(str_ary) when is_list(str_ary) do
     craft_name_calling_message("Hey! ", str_ary)
   end
 
+  @type name() :: String.t()
+
+  @spec craft_name_calling_message(string(), list()) :: string()
   def craft_name_calling_message(insult, [name | tail]) do
     insult <> name <> "! You're " <> Enum.join(tail, " ") <> ", bruh."
+  end
+
+  def handle_message("!call " <> rest) do
   end
 
   # Ignore any other events
--- squish_bot/mix.exs
+++ squish_bot/mix.exs
@@ -23,6 +23,7 @@ defmodule SquishBot.MixProject do
   defp deps do
     [
       {:nostrum, github: "Kraigie/nostrum"},
+      {:stench, ">= 0.1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
